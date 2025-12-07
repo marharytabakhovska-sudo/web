af82e5a278906291619ba3fd4d060c04e853cd2d
marharytabakhovska <marharyta.bakhovska@oa.edu.ua>
2025-12-07 18:16:31 +0200

    Лабораторна 4. Створення веб-застосунку з використанням Vue 3, TypeScript та Vue Router.

---
+++ vue-lab/src/views/ContactView.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+// 1. Імпортуємо компонент
+import UserCard from '@/components/UserCard.vue';
+</script>
+
+<template>
+  <div class="contact-page">
+    <h1>Сторінка контактів</h1>
+    <p>Це наша нова сторінка контактів!</p>
+
+    <UserCard username="Student_1" :isActive="true" />
+    <UserCard username="Student_2" :isActive="false" />
+    <!-- <UserCard username="Student_Error" :isActive="100" /> -->
+    <!--  Помилка Vue: Type 'number' is not assignable to type 'boolean'.-->
+
+  </div>
+</template>
+
+<style scoped>
+.contact-page {
+  padding: 20px;
+}
+</style>
